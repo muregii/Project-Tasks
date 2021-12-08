18c052ea4aca53ca8f1a8711d15fe3f55d65059f
muregii <88530358+muregii@users.noreply.github.com>
2021-12-08 22:32:06 +0800

    Add files via upload

---
+++ struct in C.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+
+typedef struct student
+{
+    char fname[30];
+    int age;
+    char freg[30];
+
+}student;
+
+int main()
+{
+    int d;
+    struct student details;
+    do{
+    printf("Enter the student name: ");
+    scanf("%s",details.fname);
+
+    printf("Enter the student age: ");
+    scanf("%d",&details.age);
+
+    printf("Enter the student registration number: ");
+    scanf("%s",details.freg);
+
+    printf("\nStudent details: \nName: %s\nAge: %d\nRegNo: %s",details.fname,details.age,details.freg);
+    printf("\n");
+    printf("Want to exit? \n 1. NO \t 2.YES");
+    printf("\n");
+    scanf("%d",&d);
+
+
+}while(d!=1);
+
+
+}
