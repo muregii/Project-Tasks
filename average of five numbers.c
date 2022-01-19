d91231786832bccd349e02ec741bb418f2037c44
muregii <88530358+muregii@users.noreply.github.com>
2022-01-19 20:28:04 +0300

    Add files via upload
    passing numbers to an array and averaging them. Beginner stuff but be keen.

---
+++ average of five numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int avg,sum,x,j=5;
+    int a[x];
+    for(int i=0;i<j;i++){
+    printf("Enter five numbers: ");
+    scanf("%d",&a[x]);
+    sum+=a[x];
+    avg=sum/5;
+
+}
+printf("\n Your average is %d: ",avg);
+}
